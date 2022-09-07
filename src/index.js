aca2cec9bd00100d70d2dade715e1a4e6df12029
Volodymyr Pyts <volodymyr.pyts@gmail.com>
2022-09-07 22:13:12 +0300

    Start

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import { App } from 'components/App';
+import './index.css';
+
+import { theme } from './components/theme/theme';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename="/goit-react-hw-05-movies/">
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+);
